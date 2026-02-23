00513416a1e119f7d45c2cab86ea07030365b4d9
sakshi mane <manesakshishivaji2006@gmail.com>
2026-02-23 07:14:05 +0530

    Add files via upload

---
+++ dem.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class dem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int MYage;                  // declaration
+        System.out.print("Enter age = ");
+        MYage = sc.nextInt();       // initialization (assigning value)
+
+        System.out.println("Your age = " + MYage);
+    }
+}
